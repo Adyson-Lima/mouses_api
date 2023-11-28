9f2c131f74ecf8ef21c953559b1db54572d1de7f
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-28 14:51:00 -0300

    13° commit - tdd controller: Consegue listar um mouse especifico e retornar status 200? GREEN

--- app/controllers/api/v1/mouses_controller.rb
+++ app/controllers/api/v1/mouses_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::MousesController < ApplicationController 
 
-  before_action :set_mouse, only: %i[] # show update destroy
+  before_action :set_mouse, only: %i[show] # show update destroy
 
   def index
     @mouses = Mouse.all 
     render json: @mouses
+  end
+
+  def show
+    render json: @mouse
   end
 
 private
