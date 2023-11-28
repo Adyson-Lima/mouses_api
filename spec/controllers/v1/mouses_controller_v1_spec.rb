fb8a014f7e99d1f38a99489b56c230958b6bee66
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-28 14:39:43 -0300

    10° commit - tdd controller: Consegue listar todos os mouses e retornar status 200? RED

---
+++ spec/controllers/v1/mouses_controller_v1_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::MousesController, type: :controller do
+
+  before{@mouse = Mouse.Create(mouse_type: "fixo", description: "mouse incomum")}
+
+  describe 'GET /api/v1/mouses' do
+    it 'Consegue listar todos os mouses e retornar status 200?' do
+      get :index
+      expect(JSON.parse(response.body).size).to eq(1)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+end
